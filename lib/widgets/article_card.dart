f872de5018f180b25b4a3d8e834b4becf908b2bb
Tracy <thuytk.ftu@gmail.com>
2022-06-23 11:34:58 +0100

    Make the app responsive to different screen size

--- lib/widgets/article_card.dart
+++ lib/widgets/article_card.dart
@@ -14,40 +14,112 @@ class ArticleCard extends StatelessWidget {
     return Card(
       margin: const EdgeInsets.all(16),
       elevation: 4,
-      child: Column(
-        children: [
-          CardBanner(imageUr1: article.urlToImage),
-          CardDetail(article: article),
-        ],
+      child: LayoutBuilder(
+        builder: (context, constraints) {
+          final isTablet = constraints.maxWidth > 600;
+          return isTablet
+              ? WideCard(article: article)
+              : TallCard(article: article);
+        },
       ),
+    );
+  }
+
+  // @override
+  // Widget build(BuildContext context) {
+  //   final isPortrait =
+  //       MediaQuery.of(context).orientation == Orientation.portrait;
+  //   return Card(
+  //     margin: const EdgeInsets.all(16),
+  //     elevation: 4,
+  //     child:
+  //         isPortrait
+  //           ? TallCard(article: article)
+  //           : WideCard(article: article),
+  //   );
+  // }
+}
+
+class TallCard extends StatelessWidget {
+  const TallCard({
+    Key? key,
+    required this.article,
+  }) : super(key: key);
+
+  final Article article;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        CardBanner(imageUr1: article.urlToImage),
+        CardDetail(article: article),
+      ],
+    );
+  }
+}
+
+class WideCard extends StatelessWidget {
+  const WideCard({
+    Key? key,
+    required this.article,
+  }) : super(key: key);
+
+  final Article article;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        CardBanner(
+          imageUr1: article.urlToImage,
+          isWideCard: true,
+        ),
+        Expanded(
+          child: CardDetail(article: article),
+        ),
+      ],
     );
   }
 }
 
 class CardBanner extends StatelessWidget {
   final String? imageUr1;
-  const CardBanner({Key? key, this.imageUr1}) : super(key: key);
+  final bool isWideCard;
+
+  const CardBanner({Key? key, this.imageUr1, this.isWideCard = false})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Stack(
       children: [
-        SizedBox(
-          height: 200,
-          width: double.infinity,
-          child: Image.network(
-            imageUr1!,
-            fit: BoxFit.cover,
+        ClipRRect(
+          borderRadius: const BorderRadius.only(
+            topLeft: Radius.circular(4),
+            topRight: Radius.circular(4),
+          ),
+          child: SizedBox(
+            height: isWideCard ? 150 : 200,
+            width: isWideCard ? 150 : double.infinity,
+            child: Image.network(
+              imageUr1!,
+              fit: BoxFit.cover,
+              loadingBuilder: (context, child, progress) {
+                if (progress == null) return child;
+                return const Center(child: CircularProgressIndicator());
+              },
+            ),
           ),
         ),
         Positioned(
-          top: 10,
-          right: 10,
+          top: isWideCard ? 2 : 10,
+          right: isWideCard ? 2 : 10,
           child: IconButton(
             icon: const Icon(Icons.bookmark_border, size: 32),
             onPressed: () {},
           ),
-        )
+        ),
       ],
     );
   }
@@ -60,22 +132,21 @@ class CardDetail extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
+    final isPortrait =
+        MediaQuery.of(context).orientation == Orientation.portrait;
+
+    return Container(
+      height: isPortrait ? 130 : 150,
       padding: const EdgeInsets.all(16),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: <Widget>[
           Text(
             article!.title,
-            //style: const TextStyle(fontSize: 24),
-            //style: Theme.of(context).textTheme.headline5!.copyWith(
-            //     fontStyle: FontStyle.italic,
-            //     color: Colors.grey[600],
             style: Theme.of(context).textTheme.headline5,
             maxLines: 2,
             overflow: TextOverflow.ellipsis,
           ),
-          const SizedBox(height: 16),
           Row(
             children: <Widget>[
               Text(article!.source),
